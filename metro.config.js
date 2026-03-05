3e7a001b50365c45754cfd1e9cfb0ad7b07e61b0
project-3 <your-github-pruthvirajul2001@gmail.com@example.com>
2026-03-05 10:10:35 +0530

    push

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,6 @@
 
 const { getDefaultConfig } = require('@expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-// Metro should treat project root as this workspace
-config.projectRoot = __dirname;
-config.watchFolders = [];
-
-// Add blockList rules
-config.resolver.blockList = [
-  /@expo[\\/]cli[\\/].*/,
-  /node_modules[\\/].*?[\\/]node_modules[\\/]@expo[\\/]cli[\\/].*/,
-  /@expo[\\/]cli[\\/]build[\\/]metro-require[\\/]require\.js$/,
-];
 
 module.exports = config;
